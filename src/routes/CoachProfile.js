fee91938ec89433de28c31071e0624ddbedffb26
Osha Foster <oshafoster@hotmail.com>
2018-01-27 16:14:04 -0700

    small changes

--- src/routes/CoachProfile.js
+++ src/routes/CoachProfile.js
@@ -21,26 +21,27 @@ class CoachProfile extends React.Component {
 
         <div className="about">
             <h1>Bob Thomas<span style={{fontSize:'16px', color:'#414141'}}> , Male</span></h1>
-            <p><span> Specialty: </span> Life Coach</p>
-            <p><span> Location: </span> San Francisco</p>
-            <p><span> Connect: </span> thomas_bob@gmail.com</p>
-            <p><span> Request Free Session: </span> Link </p>
+            <p><span> Specialty  </span> Life Coach</p>
+            <p><span> Location  </span> San Francisco</p>
+            <p><span> Connect  </span> thomas_bob@gmail.com</p>
+            <p><span> Request Free Session  </span> Link </p>
         </div>
             <Action/>
         <div className="div2">
             <div className='videoDiv'>Video</div>
             <div className="infoDiv">
-                <h2 style={{color: "#cd5360"}}>Specialties:</h2>
-                <h3>Concerns I help with:<span> Life goals, career change, relationships </span></h3>
-                <h3>Experience/Education:<span> MD Social Sciences, Studied Life Coaching with David Gray </span> </h3>
-                <h3>Background:<span> Lorem ipsum dolor sit amet,
+                <div><h2 style={{color: "#cd5360"}}>Specialties</h2></div>
+                <div><h3>Concerns I help with</h3>
+            <p>Live your best life. Find a passionate relationship. Find the career that feeds your soul. </p></div>
+            <h3>Experience/Education<p> MD Social Sciences, Studied Life Coaching with David Gray </p> </h3>
+        <h3>Background:<p> Lorem ipsum dolor sit amet,
                                 adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
                                 ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                consequat</span></h3>
-                <h3>Methods of coaching:<span> In person, phone, video </span></h3>
-                <h3>Languages:<span> English </span></h3>
-                <h3>Form of payment:<span> Credit, Debit, Check </span></h3>
-                <h3>Hourly rate:<span> 45$ hourly </span></h3>
+                                consequat</p></h3>
+                            <h3>Methods of coaching <p> In person, phone, video</p></h3>
+                        <h3>Languages:<p> English </p></h3>
+                    <h3>Form of payment:<p> Credit, Debit, Check </p></h3>
+                <h3>Hourly rate:<p> 45$ hourly </p></h3>
 
 
             </div>
